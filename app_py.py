5a5a442fbd3471d25836b988bb74c470257d8f3f
SANAUL MUSTAFA <95904674+msanaul@users.noreply.github.com>
2022-09-19 15:57:18 +0530

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -23,5 +23,5 @@ def predict():
               return render_template('index.html',prediction_text = "You are Fine.")
         else:
               return render_template('index.html',prediction_text = "You need treatment.")
-
-app.run(debug=True)
+if __name__ == '__main__':
+     app.run(debug=True)
